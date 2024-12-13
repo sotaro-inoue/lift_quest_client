9604c0c8a4bf7eeeff248429cb0b466845866f58
sotaro-inoue <ino.s0212@gmail.com>
2024-12-13 12:49:49 +0900

    機能説明のカードを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import { FunctionDescriptionCard1, FunctionDescriptionCard2, FunctionDescriptionCard3 } from "@/components/ui/FunctionDescriptionCard";
 
 export default function Home() {
   return (
@@ -8,9 +8,14 @@ export default function Home() {
           <h1 className="font-mobobold text-9xl">LiftQuest</h1>
           <h2 className="font-mobosemibold text-2xl">トレーニングの世界を、一緒に冒険しよう。</h2>
         </div>
-        <div className="flex items-center justify-center flex-col h-screen gap-4 p-4 space-y-4">
-          <h2 className="font-mobobold text-5xl">LiftQuestでできること</h2>
+        <div className="flex items-center justify-center flex-col h-screen gap-4 p-6 space-y-4">
           <div>
+            <h2 className="font-mobobold text-5xl">LiftQuestでできること</h2>
+          </div>
+          <div className="flex flex-row gap-5 size-full p-10">
+            <FunctionDescriptionCard1 />
+            <FunctionDescriptionCard2 />
+            <FunctionDescriptionCard3 />
           </div>
         </div>
       </main>
